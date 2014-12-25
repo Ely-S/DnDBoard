3afea8e95d25d3bd78ae48913ad31761e5e0c67a
eli <eli.sakov@hotmail.com>
2014-12-25 13:50:07 -0500

    time to use git

---
+++ data.js
@@ -0,0 +1,22 @@
+data = {
+	pieces: ["pc.jpg"],
+	maps: [
+		{
+			name: "Field",
+			src: "map.png",
+			twidth: 32,
+			theight: 32,
+			height: 100,
+			width: 100
+		},
+		{
+			name: "Preview",
+			src: "preview.png",
+			twidth: 32,
+			theight: 32,
+			height: (894/32)-1,
+			width: 864/32
+		}
+
+	]
+};
---
+++ script.js
@@ -0,0 +1,98 @@
+jQuery(function($){
+"use strict";
+var Map = $("#map"), // Map Element
+	MapHolder = $("#map-holder"), // Map Holder element
+	map; // map object
+
+
+// Setup
+
+data.pieces.forEach(function(src, index){
+	$("<img>").attr({
+		"class": "piece draggable",
+		"src": "pieces/" + src,
+		"draggable": "true",
+		"id": "piece" + index
+	})
+	.appendTo("#pieces-holder")
+	.on("dragstart", function(e){
+      e.originalEvent.dataTransfer.effectAllowed = 'copy'; // only dropEffect='copy' will be dropable
+      e.originalEvent.dataTransfer.setData('text', this.id); // required otherwise doesn't work
+	  this.classList.add("floating");
+	})
+});
+
+data.maps.forEach(function(m){
+	$("<option>").text(m.name)
+	.attr("value", m.src)
+	.appendTo("#map-picker");
+});
+
+
+// Events
+
+$("#reload-btn").click(function(){
+
+});
+ 
+ $("#reset-btn").click(function(){
+ 	$(".piece").appendTo("#pieces-holder");
+ });
+
+$("#map-picker").change(function(){
+	var value = this.value;
+	Map.css("background-image", "url(maps/" + value + ")");
+	$(".tiles").remove();
+	map = data.maps.filter(function(map){
+		return value == map.src; 
+	})[0];
+	var frag = document.createDocumentFragment(),
+		width = map.twidth + "px",
+		height = map.theight + "px",
+		t, tilesNumber = Math.round(map.height*map.width);
+	while (tilesNumber-- >= 0) {
+		t = new Tile(tilesNumber, width, height);
+		frag.appendChild(t.el);
+	}
+	Map.height(map.height * map.theight)
+		.width(map.width * map.twidth)
+		.append(frag);
+});
+
+// Dragand Drop
+
+Map.on(".tile", "dragover", function(e){
+ 	if (e.preventDefault) e.preventDefault(); // allows us to drop
+    e.originalEvent.dataTransfer.dropEffect = 'copy';
+	return false;	
+}).on(".tile", "dragenter", function(e){
+	return false;
+}).on(".tile", "dragover", function(){
+	return false;
+}).on(".tile", "drop", function(e){
+	if (e.stopPropagation) e.stopPropagation(); // stops the browser from redirecting...why???
+
+    var src = e.originalEvent.dataTransfer.getData("text");
+
+    $(".piece").filter(function(){
+    	return this.src==src;
+    }).appendTo(this);
+
+    return false;
+});
+
+
+$("#map-picker").val(data.maps[1].src).trigger("change");
+
+function Tile (num, width, height) {
+  this.el = document.createElement("div");
+  this.el.setAttribute("id", num);
+  this.el.className = "tile";
+  this.el.textContent = "\n";
+  this.el.style.width = width;
+  this.el.style.height = height;
+
+}
+
+
+});
